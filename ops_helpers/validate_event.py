dc811cfc547579d3a64e01d60a7a5c8a0c5c8379
Dave Fort <dhfort@gmail.com>
2018-10-10 12:52:26 -0700

    Remove schema path definition

--- ops_helpers/validate_event.py
+++ ops_helpers/validate_event.py
@@ -1,23 +1,12 @@
 import json
-from os import path, getcwd
 from jsonschema import validate
 from jsonschema.exceptions import ValidationError
 
-WORKDIR = getcwd()
-
 
 # [START event validation]
-# NB: Tightly coupled to layout of Operations-FAAS directory
 def validate_event(event, schema: str) -> None:
     "Compare event input to object required by schema"
-    SCHEMA = path.abspath(
-        path.join(
-            path.dirname(WORKDIR),
-            f'../functions/schemas//{schema}'   
-        )
-    )
-
-    with open(SCHEMA) as schema_file:
+    with open(schema) as schema_file:
         schema = json.load(schema_file)
     try:
         validate(event, schema)
