3d27d4cd8837445a37a5c4e05a85f27f6b7b2d3a
Dave Fort <dhfort@gmail.com>
2018-10-10 13:50:58 -0700

    simplify schema read logic

--- ops_helpers/validate_event.py
+++ ops_helpers/validate_event.py
@@ -1,12 +1,14 @@
 import json
+from os import path
 from jsonschema import validate
 from jsonschema.exceptions import ValidationError
 
 
 # [START event validation]
-def validate_event(event, schema: str) -> None:
-    "Compare event input to object required by schema"
-    with open(schema) as schema_file:
+def validate_event(event, schema_path: str) -> None:
+    SCHEMA = path.join(path.dirname(__file__), schema_path)
+
+    with open(SCHEMA) as schema_file:
         schema = json.load(schema_file)
     try:
         validate(event, schema)
