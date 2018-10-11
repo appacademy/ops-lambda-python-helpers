53f006f656e5c946eb218f7484efbff0de72d66d
Dave Fort <dhfort@gmail.com>
2018-10-11 15:06:14 -0700

    exclude test from packages; try tuple

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     description="Helper functions for use with ops lambda functions",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=find_packages(exclude=["test"]),
+    packages=find_packages(exclude="test",),
     incude_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
