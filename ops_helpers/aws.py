8236675a2c14cc28ba6f374409ae39bf7d26a41f
Dave Fort <dhfort@gmail.com>
2018-10-26 18:53:20 -0700

    Fail silently if secret is None

--- ops_helpers/aws.py
+++ ops_helpers/aws.py
@@ -24,25 +24,29 @@ def get_secret_file(secret_name: str, region_name: str) -> Dict:
 
 
 def fetch_secrets(secret_name: str, region_name: str) -> None:
-    "Get secrets from AWS Secrets Manager; only works if AWS credentials are active env variables"
+    """"Get secrets from AWS Secrets Manager
+    Only works if AWS credentials are active env variables"""
+    if secret_name is None:
+        return None
+
     try:
         get_secret_value_response = get_secret_file(secret_name, region_name)
     except ClientError as e:
         if e.response['Error']['Code'] == 'DecryptionFailureException':
             raise e(
-                "Secrets Manager can't decrypt the protected secret text using the provided KMS key.")
+                "Can't decrypt the protected secret text with provided KMS key.")
         elif e.response['Error']['Code'] == 'InternalServiceErrorException':
             raise e('An error occurred on the server side.')
         elif e.response['Error']['Code'] == 'InvalidParameterException':
             raise e('You provided an invalid value for a parameter.')
         elif e.response['Error']['Code'] == 'InvalidRequestException':
             raise e(
-                'You provided a parameter value that is not valid for the current state of the resource.')
+                'You provided an invalid parameter value for state of the resource.')
         elif e.response['Error']['Code'] == 'ResourceNotFoundException':
             raise e('Resource cannot be found')
     else:
         # Decrypts secret using the associated KMS CMK, if applicable.
-        # Depending on whether secret is a string or binary, one of these fields will be populated.
+        # Inserts all secerts as environment variables
         if 'SecretString' in get_secret_value_response:
             secrets = json.loads(get_secret_value_response['SecretString'])
             for key, value in secrets.items():
